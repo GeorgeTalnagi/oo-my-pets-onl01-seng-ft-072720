ba7cad78c303ac9b65bb1a52bed9075506c3430e
George Talnagi <gtalnagi@yahoo.com>
2020-08-12 02:57:55 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -45,8 +45,8 @@ class Owner
 #can buy a cat that is an instance of the Cat class, knows about its cats
   def buy_cat(name)
     #passing this owner object into the cat object
-    cat = Cat.new
-    @cats << Cat.new(name, self) 
+    cat = Cat.new(name, self) 
+    @cats << cat
     
   end 
   
