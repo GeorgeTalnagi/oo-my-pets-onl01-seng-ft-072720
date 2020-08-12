c598e6f63d90f0e0e0a679fcde9e6a551990580d
George Talnagi <gtalnagi@yahoo.com>
2020-08-12 02:17:26 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,24 +1,5 @@
 class Cat
-  attr_accessor :owner, :mood, :cats 
-  attr_reader :name
- 
- @@all = []
- 
-  
-  def initialize(name, owner)
-    @name = name
-    @owner = owner
-    @mood = "nervous"
-  end 
 
-  def save 
-    @@all << self 
-  end 
-  
-  def self.all 
-    @@all
-  end 
-  
   
 end 
 
