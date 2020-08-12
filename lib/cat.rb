9e8ec2c55147a7ee31903ba6c68df7744e38358a
George Talnagi <gtalnagi@yahoo.com>
2020-08-12 02:25:43 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -6,9 +6,9 @@ attr_reader :name
   
   
   def initialize(name, owner)
-  @name = name 
-  @owner = owner 
-  @mood = "nervous"
+    @name = name 
+    @owner = owner 
+    @mood = "nervous"
   end 
 
   def self.all
